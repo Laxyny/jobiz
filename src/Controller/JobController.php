ebb50b47f5e776f63c7f33cba372c03564b095cb
Laxyny <kevgreg30@gmail.com>
2025-06-05 14:23:09 +0200

    *ADD* Affichage en détail de une offre d'emploi

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Job;
 use App\Repository\JobRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,6 +17,14 @@ final class JobController extends AbstractController
 
         return $this->render('job/list.html.twig', [
             'jobs' => $jobs,
+        ]);
+    }
+
+    #[Route('/job/{id}', name: 'app_job_show')]
+    public function show(Job $job): Response
+    {
+        return $this->render('job/show.html.twig', [
+            'job' => $job,
         ]);
     }
 }
